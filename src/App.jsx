930dd5ea653a5dc761a1bbeaa93866779a517752
jaebeen <jbeen.dev@gmail.com>
2024-09-08 01:27:57 +0800

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { create } from 'zustand'
+import Scene from './components/Scene'
 
 const useStore = create((set) => ({
   count: 1,
   inc: () => set((state) => ({ count: state.count + 1 })),
 }))
 
-function Counter() {
+function Long() {
   const { count, inc } = useStore()
   return (
     <div className="counter">
@@ -15,16 +16,30 @@ function Counter() {
   )
 }
 
+function Short() {
+  const { count, inc } = useStore()
+  return (
+    <div className="counter">
+      <span>{count}</span>
+      <button onClick={inc}>Short</button>
+    </div>
+  )
+}
+
 export default function App() {
   return (
     <>
+      <Scene />
       <div className="main">
-        <div className="code">
-          <div className="code-container">
-            <Counter />
+        <div className="vote">
+          <div className="vote-container">
+            <Long />
           </div>
+          <div className="vote-container">
+            <Short />
+          </div>          
         </div>
-      </div>
+      </div>    
     </>
   )
 }
